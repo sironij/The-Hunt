63efa4941b2323c0a0ae63ca6f47118c14efde2d
MarcoValli <marco.valli@gmail.com>
2021-01-27 17:32:07 +0100

    Test js

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@ $(document).scroll(function() {
   $("#scroll").fadeOut("slow");
 });
 
-$(document).ready(function() {
+
 
   // prevent refresh
 
@@ -123,33 +123,33 @@ if ('scrollRestoration' in history) {
 
 
   // 3D effect finestre -> da valutare
-  $('html').mousemove(function(e) {
-
-    var wx = $(window).width();
-    var wy = $(window).height();
-
-    var x = e.pageX - this.offsetLeft;
-    var y = e.pageY - this.offsetTop;
-
-    var newx = x - wx / 2;
-    var newy = y - wy / 2;
-
-    $('span').text(newx + ", " + newy);
-
-    $('#wrapper div').each(function() {
-      var speed = $(this).attr('data-speed');
-      if ($(this).attr('data-revert')) speed *= -1;
-      TweenMax.to($(this), 1, {
-        x: (1 - newx * speed),
-        y: (1 - newy * speed)
-      });
-
-    });
-
-  });
-
-
-})
+//  $('html').mousemove(function(e) {
+//
+//    var wx = $(window).width();
+//    var wy = $(window).height();
+//
+//    var x = e.pageX - this.offsetLeft;
+//    var y = e.pageY - this.offsetTop;
+//
+//    var newx = x - wx / 2;
+//    var newy = y - wy / 2;
+//
+//    $('span').text(newx + ", " + newy);
+//
+//    $('#wrapper div').each(function() {
+//      var speed = $(this).attr('data-speed');
+//      if ($(this).attr('data-revert')) speed *= -1;
+//      TweenMax.to($(this), 1, {
+//        x: (1 - newx * speed),
+//        y: (1 - newy * speed)
+//      });
+//
+//    });
+//
+//  });
 
 
-;
+
+
+
+
