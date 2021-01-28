491d63bc88bfdbeafc9ae3cb87564d663a3ac15e
MarcoValli <marco.valli@gmail.com>
2021-01-28 16:23:05 +0100

    minor bugs and new js for wb

---
+++ js/wb.js
@@ -0,0 +1,12 @@
+
+$(".wb-el").click(function(){
+     
+     var titolo = $(this).textContent;
+     
+    $('#wb-title').empty();
+    $('#wb-txt').empty();
+    $('#wb-title').text(titolo);
+     
+     
+ })
+         
